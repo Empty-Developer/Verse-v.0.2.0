24a365fad5f58a799ed741c46fed6658b384238c
Empty <emptypageworking@gmail.com>
2026-06-23 15:30:50 +0700

    Feat: create registration date for supabase
    now save data in async and supabe

--- stores/useAuthStore.ts
+++ stores/useAuthStore.ts
@@ -1,13 +1,74 @@
-import { create } from 'zustand'
+import { User } from "@/types/types";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import { supabase } from "@/lib/supabase";
 
 type AuthStore = {
-
-}
+  user: User | null;
+  isAuthenticated: boolean;
+  login: (email: string, password: string) => Promise<void>;
+  register: (
+    email: string,
+    password: string,
+    username: string,
+  ) => Promise<void>;
+  logout: () => Promise<void>;
+};
 
 /**
  * @description this function - create
  * a new type data user useAuthStore(name example)
+ * @returns {object}
  */
 export const useAuthStore = create<AuthStore>()(
+  // save state
+  persist(
+    (set, get) => ({
+      user: null,
+      isAuthenticated: false,
+      login: async (email: string, password: string) => {},
+      register: async (email: string, password: string, username: string) => {
+        // check append user in store
+        try {
+          // passing on the information
+          const { data, error } = await supabase.auth.signUp({
+            email,
+            password,
+            options: {
+              data: {
+                username
+              }
+            }
+          })
 
-)
+          if (data && data.user && !error) {
+            const { user } = data;
+            /**
+             * @description this function create
+             * object user and save in data supabase
+             * @returns {object}
+             */
+            const newUser: User = {
+              id: user.id,
+              email: user.email!,
+              username: user.user_metadata.username
+            }
+
+            set({
+              user: newUser,
+              isAuthenticated: true,
+            })
+          }
+        } catch (error) {
+          throw error
+        }
+      },
+      logout: async () => {},
+    }),
+    {
+      name: "auth-storage",
+      storage: createJSONStorage(() => AsyncStorage), // save data on user device
+    },
+  ),
+);
