ef93acda3401ca4f8221bf7a79e61638c06f37c1
Empty <emptypageworking@gmail.com>
2026-06-26 16:01:04 +0700

    Fix: icon for home screen

--- app/(protected)/(tabs)/_layout.tsx
+++ app/(protected)/(tabs)/_layout.tsx
@@ -4,11 +4,11 @@ export default function TabLayout() {
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
-        <Label>Home</Label>
-        <Icon sf="house.fill" drawable="md-main" selectedColor={"#6e6a6aff"}/>
+        <Label>News</Label>
+        <Icon sf="textformat.characters" drawable="md-main" selectedColor={"#6e6a6aff"}/>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="profile">
-        <Icon sf="person.fill" drawable="md-person" selectedColor={'#6e6a6aff'}/>
+        <Icon sf="person" drawable="md-person" selectedColor={'#6e6a6aff'}/>
         <Label>Profile</Label>
       </NativeTabs.Trigger>
       {/* <NativeTabs.Trigger name="search" role='search'>
